a952ee2024cfc80f2915d599d057950d0890263d
alina <alinaotten@web.de>
2019-01-05 13:58:24 +0100

    added button in home screen view and added start screen

---
+++ screens/StartScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Colors from "../constants/Colors";
+import { StatusBar } from 'react-native';
+import { Header, Left, Right, Icon, Body, Title, Container } from 'native-base';
+
+export  class StartScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Start',
+    };
+    render() {
+        return (
+            <Container>
+                <Header style={{backgroundColor: Colors.primaryColor}}>
+                    <StatusBar barStyle='light-content'/>
+                    <Left>
+                        <Icon style={{color: Colors.primaryTextColor}} name="menu" onPress={()=>this.props.navigation.openDrawer()}/>
+                    </Left>
+                    <Body style={{flex: 2}}>
+                    <Title style={{color: Colors.primaryTextColor, fontSize: 24}}>Manguli Exquis</Title>
+                    </Body>
+                    <Right/>
+                </Header>
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
+});
