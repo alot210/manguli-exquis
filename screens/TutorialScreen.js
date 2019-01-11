bd08a60996ded8e4a8895686a668934cefb259f0
alina <alinaotten@web.de>
2019-01-11 11:25:32 +0100

    added images and styles for tutorial screen

--- screens/TutorialScreen.js
+++ screens/TutorialScreen.js
@@ -1,34 +1,77 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet, Image, ScrollView} from 'react-native';
 import Colors from "../constants/Colors";
-import { StatusBar } from 'react-native';
-import { Header, Left, Right, Icon, Body, Title, Container } from 'native-base';
-import HeaderBar from "../components/HeaderBar";
+import {StatusBar} from 'react-native';
+import {Header, Left, Right, Icon, Body, Title, Container, Text} from 'native-base';
 
 export default class TutorialScreen extends React.Component {
-  render() {
-    return (
-      <Container>
-        <HeaderBar {...this.props} title='Tutorial'/>
-        {/*<Header style={{backgroundColor: Colors.primaryColor}}>*/}
-          {/*<StatusBar barStyle='light-content'/>*/}
-          {/*<Left>*/}
-            {/*<Icon style={{color: Colors.primaryTextColor}} name="menu" onPress={()=>this.props.navigation.openDrawer()}/>*/}
-          {/*</Left>*/}
-          {/*<Body style={{flex: 2}}>*/}
-          {/*<Title style={{color: Colors.primaryTextColor, fontSize: 24}}>Tutorial</Title>*/}
-          {/*</Body>*/}
-          {/*<Right/>*/}
-        {/*</Header>*/}
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Header style={{backgroundColor: Colors.primaryColor}}>
+                    <StatusBar barStyle='light-content'/>
+                    <Left>
+                        <Icon style={{color: Colors.primaryTextColor}} name="menu"
+                              onPress={() => this.props.navigation.openDrawer()}/>
+                    </Left>
+                    <Body style={{flex: 2}}>
+                    <Title style={{color: Colors.primaryTextColor, fontSize: 24}}>Tutorial</Title>
+                    </Body>
+                    <Right/>
+                </Header>
+                <ScrollView style={{flex: 1}}>
+                    <Text style={{
+                        alignSelf: 'center',
+                        paddingBottom: 32,
+                        paddingTop: 20,
+                        color: Colors.primaryTextColor,
+                        fontSize: 24
+                    }}>1. Wähle einen Raum</Text>
+                    <Text style={{alignSelf: 'center', paddingBottom: 20}}>Wähle zunächst einen existierenden Raum oder
+                        erstelle deinen eigenen Raum.</Text>
+                    <Image source={require('../assets/images/multiple-users-silhouette.png')}
+                           style={{width: 80, height: 80, alignSelf: 'center'}}></Image>
+                    <Text style={{
+                        alignSelf: 'center',
+                        paddingBottom: 32,
+                        paddingTop: 50,
+                        color: Colors.primaryTextColor,
+                        fontSize: 24
+                    }}>2. Entscheide dich für ein Spiel</Text>
+                    <Text style={{alignSelf: 'center', paddingBottom: 20}}>Du hast die Möglichkeit zwischen diversen
+                        Minispielen zu wählen.</Text>
+
+                    <Image source={require('../assets/images/scrabble.png')}
+                           style={{width: 80, height: 80, alignSelf: 'center', marginBottom: 20}}></Image>
+                    <Text style={{alignSelf: 'center', paddingBottom: 40}}>Bilde verrückte Sätze!</Text>
+                    <Image source={require('../assets/images/book.png')}
+                           style={{width: 80, height: 80, alignSelf: 'center', marginBottom: 20}}></Image>
+                    <Text style={{alignSelf: 'center', paddingBottom: 40}}>Erstelle deine eigene, verrückte
+                        Geschichte!</Text>
+                    <Image source={require('../assets/images/poem.png')}
+                           style={{width: 80, height: 80, alignSelf: 'center', marginBottom: 20}}></Image>
+                    <Text style={{alignSelf: 'center'}}>Reime drauf los!</Text>
+                    <Text style={{
+                        alignSelf: 'center',
+                        paddingBottom: 32,
+                        paddingTop: 50,
+                        color: Colors.primaryTextColor,
+                        fontSize: 24
+                    }}>3. Beginne das Spiel</Text>
+                    <Text style={{alignSelf: 'center', paddingBottom: 20}}>Mit dem Start-Button beginnst du das Spiel.
+                        Um mehr Informationen über das Spiel zu erhalten benutze den Informations-Button.</Text>
+                    <Image source={require('../assets/images/info.png')}
+                           style={{width: 50, height: 50, alignSelf: 'center', marginBottom: 50}}></Image>
+                </ScrollView>
+            </Container>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
 });
