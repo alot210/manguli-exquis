d0850b2cfa20b513f099e5948fc6e5ab3e2d02d0
alina <alinaotten@web.de>
2019-01-15 11:51:55 +0100

    added headerbar

--- screens/TutorialScreen.js
+++ screens/TutorialScreen.js
@@ -3,22 +3,14 @@ import {StyleSheet, Image, ScrollView} from 'react-native';
 import Colors from "../constants/Colors";
 import {StatusBar} from 'react-native';
 import {Header, Left, Right, Icon, Body, Title, Container, Text} from 'native-base';
+import HeaderBar from "../components/HeaderBar";
 
 export default class TutorialScreen extends React.Component {
+
     render() {
         return (
             <Container>
-                <Header style={{backgroundColor: Colors.primaryColor}}>
-                    <StatusBar barStyle='light-content'/>
-                    <Left>
-                        <Icon style={{color: Colors.primaryTextColor}} name="menu"
-                              onPress={() => this.props.navigation.openDrawer()}/>
-                    </Left>
-                    <Body style={{flex: 2}}>
-                    <Title style={{color: Colors.primaryTextColor, fontSize: 24}}>Tutorial</Title>
-                    </Body>
-                    <Right/>
-                </Header>
+                <HeaderBar {...this.props} title='Tutorial'/>
                 <ScrollView style={{flex: 1}}>
                     <Text style={{
                         alignSelf: 'center',
