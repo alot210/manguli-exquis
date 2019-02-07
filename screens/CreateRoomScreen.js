5b2379ceb2f4851e4588157b25b077eda0a172b7
Jens <jensroemer@yahoo.de>
2019-02-07 13:01:37 +0100

    add logic for joining and creating a room
    add forms and buttons

--- screens/CreateRoomScreen.js
+++ screens/CreateRoomScreen.js
@@ -1,36 +1,84 @@
 import React from 'react';
 import HeaderBar from "../components/HeaderBar";
+import Colors from "../constants/Colors";
+import { View, Alert } from 'react-native';
+import { Container, Content, Form, Label, Input, Item, Button, Text } from 'native-base';
 import firebase from '../constants/FirebaseConfig';
 
 export default class CreateRoomScreen extends React.Component {
-  static navigationOptions = {
-    drawerLabel: () => null
-  };
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "Raumname",
+            password: "",
+            user_id: 1}
+    }
 
-  createRoom() {
+
+  /*static navigationOptions = {
+    drawerLabel: () => null
+  };*/
+
+  createRoom(_that) {
     let _roomID = 1;
-    let _passwort = "Passwort";
-    let _name = "Name";
+    let _passwort = this.state.password;
+    let _name = this.state.name;
     let _link = "";
-    let _mode = "word";
     let possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
 
     for(let i = 0; i < 5; i++){
         _link = _link + possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
     }
 
-    firebase.database().ref('room/' + _roomID).set({
-        roomID: _roomID,
-        passwort: _passwort,
-        name: _name,
-        link: _link,
-        mode: _mode
-    });
+    let allRooms = firebase.database().ref('room/');
+      allRooms.once('value', function (snapshot) {
+          snapshot.forEach(function (item) {
+              _roomID = item.child("roomID").val();
+          });
+
+          //ID hochzählen für nächsten Raum
+          _roomID = _roomID + 1;
+
+          firebase.database().ref('room/' + _roomID).set({
+              roomID: _roomID,
+              passwort: _passwort,
+              name: _name,
+              link: _link,
+          });
+
+          Alert.alert(
+              "Ihr Raum wurde erstellt",
+              "Der Link zu Ihrem Raum lautet: " + _link,
+              [
+                  {text: "OK", onPress: () => ""},
+              ],
+              {cancelable: false},
+          );
+      });
   }
 
   render() {
     return (
-      <HeaderBar {...this.props} title='Raum Erstellung' />
+      <Container>
+          <HeaderBar {...this.props} title='Raum Erstellung' />
+          <Content>
+              <Form style={{marginTop: 64}}>
+                  <Item floatingLabel>
+                      <Label>Username</Label>
+                      <Input value={this.state.name} onChangeText={(name) => this.setState({name})} />
+                  </Item>
+                  <Item floatingLabel last>
+                      <Label>Password</Label>
+                      <Input value={this.state.password} onChangeText={(password) => this.setState({password})} />
+                  </Item>
+              </Form>
+              <Button
+                  style={{alignSelf: 'center'}}
+                  onPress={() => this.createRoom(this)}>
+                  <Text>Raum erstellen</Text>
+              </Button>
+          </Content>
+      </Container>
     );
   }
 }
--- screens/JoinRoomScreen.js
+++ screens/JoinRoomScreen.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import HeaderBar from "../components/HeaderBar";
 import firebase from '../constants/FirebaseConfig';
+import Colors from "../constants/Colors";
+import { View, Alert } from 'react-native';
+import { Container, Content, Form, Label, Input, Item, Button, Text } from 'native-base';
 
 export default class JoinRoomScreen extends React.Component {
-  static navigationOptions = {
+    constructor(props){
+        super(props);
+        this.state = {
+            link: "Link hier einfügen",
+            user_id: 1}
+    }
+
+  /*static navigationOptions = {
     drawerLabel: () => null
-  };
+  };*/
 
     enterRoom() {
-        let _link = "crypt";
+        let _link = this.state.link;
         let data = [];
         let found = false;
         let allRooms = firebase.database().ref('room/');
         allRooms.once('value', function (snapshot) {
-            snapshot.foreach(function (item) {
+            snapshot.forEach(function (item) {
                 data.push(item.child("link").val());
             });
 
@@ -31,10 +41,24 @@ export default class JoinRoomScreen extends React.Component {
         });
     }
 
-
   render() {
     return (
-      <HeaderBar {...this.props} title='Raum beitreten' />
+      <Container>
+        <HeaderBar {...this.props} title='Raum beitreten' />
+        <Content>
+          <Form style={{marginTop: 64}}>
+            <Item floatingLabel>
+                <Label>Link einfügen</Label>
+                <Input value={this.state.link} onChangeText={(link) => this.setState({link})} />
+            </Item>
+          </Form>
+          <Button
+            style={{alignSelf: 'center'}}
+            onPress={() => this.enterRoom()}>
+            <Text>Raum beitreten</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
